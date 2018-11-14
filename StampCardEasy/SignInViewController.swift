58d60b22e3642c009f0c9f947930ad2211e42d0c
Bilalfakhro <bilal.fakhro@hotmail.se>
2018-11-14 15:18:05 +0100

    update

--- StampCardEasy/SignInViewController.swift
+++ StampCardEasy/SignInViewController.swift
@@ -22,7 +22,6 @@ class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-  
     }
     
     override func didReceiveMemoryWarning() {
